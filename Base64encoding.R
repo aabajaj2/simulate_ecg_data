87320220660f07e0fe769a029282e93865dc327d
Anjani Bajaj <aabajaj2@dons.usfca.edu>
2017-11-21 19:27:11 -0800

    added function for packing vecto of length n

--- Base64encoding.R
+++ Base64encoding.R
@@ -3,18 +3,25 @@ ecg$Time <- ecg$Time - min(ecg$Time)
 summary(ecg)
 
 # Subtract the max negative value to make the dataset non negative
-c<-(-874)
-ecg[,] <- t(apply(ecg[,], 1, function(x) x-c))
-summary(ecg)
+# given a vector of 125 values, pack them using the same function and unpack them too.
+# Same tests, important tests are near the boundaries.
+# put those tests in the vector packing function,
+# check which ones pass and which fail 
+# random vector-> pack and unpack them and make sure they are still the same.
 
-plot_timespan <- function(t1, t2, ecg_data){
-  with(ecg_data[ecg_data$Time >=t1 & ecg_data$Time < t2,],
-       plot(ec ~ Time, type='l')
-  )
-}
+c<-min(ecg$ec)
+ecg$ec <- ecg$ec-c
+summary(ecg)
+head(ecg)
+# 
+# plot_timespan <- function(t1, t2, ecg_data){
+#   with(ecg_data[ecg_data$Time >=t1 & ecg_data$Time < t2,],
+#        plot(ec ~ Time, type='l')
+#   )
+# }
 
 #Plotting the heart rate for 1 minute, 7500th obervation
-plot_timespan(0, 2725, ecg)
+# plot_timespan(0, 2725, ecg)
 
 # Here 7500 stands for 1 minute worth of data (125 * 60 * number of minutes)
 i <- 125
@@ -22,19 +29,43 @@ newecg <- ecg[1:i,]
 object.size(newecg$ec)
 summary(newecg)
 
-library(caTools)
-encoding1 <- base64encode(newecg$ec, size=NA, endian=.Platform$endian)
-nchar(encoding1)
-encoding1
-
 pack2values <- function(v1, v2){ # must be non-negative integers in 12-bit range
   mask8 <- 255
   bitvec <- bitwShiftL(v1, 12) + v2 # 24 bit: v1v2
   as.raw(c(bitwAnd(bitwShiftR(bitvec, 16), mask8), bitwAnd(bitwShiftR(bitvec, 8), mask8), bitwAnd(bitvec, mask8)))
-  # 24 bits 0111000110010001011001010 -> c("01110001", "10010001", "11001010") # 8 bit values
 }
 
+newpack <- function(vec){
+  count <- 1
+  pack <- c()
+  answer <- c()
+  for (value in vec){
+    pack<-append(pack,value)
+    if(count%%2==0){
+      res<-pack2values(pack[1],pack[2])
+      answer <- append(answer,res)
+      #print(answer)
+      #print("HI")
+      pack <- c()
+    }
+    count <- count +1
+  }
+  return(answer)
+}
 
+pack2values(newecg$ec[1], newecg$ec[2])
+packedVector <- newpack(newecg$ec)
+packedVector
+
+library(caTools)
+encodedVector <- base64encode(packedVector, size=NA, endian=.Platform$endian)
+nchar(encodedVector)
+encodedVector
+typeof(packedVector)
+
+decodedVector <- base64decode(encodedVector, raw)
+nchar(decodedVector)
+decodedVector
 
 # 125 integer 12 bit -> how many base64 characters can it be encoded to, How short can I make it?
 # Subtract 2000 to make a unifrom range, make them all positive 
@@ -49,16 +80,43 @@ unpack2values <- function(pv){ # 3 8 bit values
   pv1 <- as.integer(pv[1])
   pv2 <- as.integer(pv[2])
   pv3 <- as.integer(pv[3])
-  
   res <- bitwAnd(bitwShiftL(bitwAnd(pv1, mask12), 4), makeLast40s) + bitwAnd(bitwShiftR(pv2,4), mask4) 
-  
   res2 <- bitwShiftL(bitwAnd(pv2, mask4), 8) + pv3
-  
   c(res, res2)
-
 }
 
-ans <- pack2values(190,100)
+newunpack <- function(vec){
+  count <- 1
+  unpack<- c()
+  result <- c()
+  for(value in vec){
+    unpack <- append(unpack, value)
+    if(count%%3 == 0){
+      answer <- unpack2values(unpack)
+      result <- append(unpack, answer)
+      print(unpack)
+      print(result)
+      print(count)
+      unpack <-c()
+    }
+    count <- count + 1
+  }
+  return(result)
+}
+
+originalVector <- newunpack(decodedVector)
+originalVector
+
+# Write a series of tests for checking the unpacking
+testVector <- round(runif(n = 20, min =0 , max = 2800))
+testVector
+for (i in seq_along(testVector)){
+  testPack <-pack2values(testVector[i], testVector[i+1])
+  print(testPack)
+  testUnpack <-unpack2values(testPack)
+  print(testUnpack)
+}
+# after I get the pack working for length N
+all.equal(testVector, testUnpack)
+ans <- pack2values(4096,6)
 unpack2values(ans)
-
-
